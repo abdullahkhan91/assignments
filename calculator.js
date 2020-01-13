c430cbaa6cdcaaa72bf641cd494aa0bc7a947cf7
abdullahkhan91 <abdullah.ali91@gmail.com>
2020-01-13 08:51:55 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//function for multiplication
+function multiply(a,b) { 
+    return a * b
+}
+
+//function for division
+function divide(a,b) {
+	return a / b 
+}
+
+//function for addition
+function add(a,b) { 
+    return a + b
+}
+
+//function for subtraction
+function add(a,b) { 
+    return a - b
+}
