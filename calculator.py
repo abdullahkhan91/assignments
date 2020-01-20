451ccd62744420e60d0f7c7209de68b375d4f559
abdullahkhan91 <abdullah.ali91@gmail.com>
2020-01-20 10:37:30 -0500

    add arithmetic functions to calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+# multiplication function
+def multiply(x,y):
+    return x * y
+
+# division function
+def divide(x,y):
+    return x / y
+
+# addition function
+def add(x,y):
+    return x + y
+
+# subtraction function
+def add(x,y):
+    return x - y
