d2fa0ae80b43e5fcc7669ab35be0fd1368ed677a
abdullahkhan91 <abdullah.ali91@gmail.com>
2020-01-20 10:46:59 -0500

    update calculator script in python to print out the numbers after the operation

--- calculator.py
+++ calculator.py
@@ -12,5 +12,22 @@ def add(x,y):
     return x + y
 
 # subtraction function
-def add(x,y):
+def subtract(x,y):
     return x - y
+
+print("This calculator function will multiply 8 and 9")
+x = multiply(8,9)
+print(x)
+
+print("This calculator function will divide 8 and 9")
+x = divide(8,9)
+print(x)
+
+print("This calculator function will add 8 and 9")
+x = add(8,9)
+print(x)
+
+print("This calculator function will subtract 9 from 8")
+x = subtract(8,9)
+print(x)
+
